37f2621ba88e9010543b600d3169720b4a8d76f2
DarrenChangJR <darrenchangjr01@gmail.com>
2024-06-04 23:25:23 +0800

    DNS over TLS with actual wifi

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,10 @@
   networking = {
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
     hostName = "nixos";
+    networkmanager = {
+      enable = true;
+      # dns = "none";
+    };
   };
 
   services = {
