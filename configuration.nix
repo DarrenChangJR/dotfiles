c2c0475511b77b11c05160331c0f7db020f3ea11
DarrenChangJR <darrenchangjr01@gmail.com>
2025-02-03 11:12:29 +0800

    add: fcitx5 config, remove: firefox

--- configuration.nix
+++ configuration.nix
@@ -41,7 +41,28 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-hangul ];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-rime
+        fcitx5-hangul
+      ];
+      settings = {
+        globalOptions = {
+          "Hotkey/TriggerKeys" = { "0" = "Super+space"; };
+        };
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "gb";
+            DefaultIM = "keyboard-gb";
+          };
+          "Groups/0/Items/0".Name = "keyboard-gb";
+          "Groups/0/Items/1".Name = "rime";
+          "Groups/0/Items/2".Name = "hangul";
+          GroupOrder."0" = "Default";
+        };
+      };
+    };
   };
 
   fonts = {
@@ -115,6 +136,7 @@
       grim
       slurp
       wl-clipboard
+      brave
     ];
   };
 
@@ -138,103 +160,6 @@
         };
         init.defaultBranch = "main";
         core.editor = "code --wait --new-window";
-      };
-    };
-    firefox = {
-      enable = true;
-      package = pkgs.firefox-esr;
-      languagePacks = [ "en-US" ];
-      policies = {
-        DisableTelemetry = true;
-        DisableFirefoxStudies = true;
-        EnableTrackingProtection = {
-          Value= true;
-          Locked = true;
-          Cryptomining = true;
-          Fingerprinting = true;
-          Exceptions = [];
-        };
-        FirefoxHome = {
-          Search = false;
-          TopSites = false;
-          SponsoredTopSites = false;
-          Highlights = false;
-          Pocket = false;
-          SponsoredPocket = false;
-          Snippets = false;
-          Locked = false;
-        };
-        DNSOverHTTPS.Enabled = true;
-        DisablePocket = true;
-        DisableFirefoxAccounts = true;
-        DisableFirefoxScreenshots = true;
-        NoDefaultBookmarks = true;
-        OverrideFirstRunPage = "";
-        OverridePostUpdatePage = "";
-        DontCheckDefaultBrowser = true;
-        DisplayBookmarksToolbar = "newtab";
-        DisplayMenuBar = "never";
-        SearchBar = "unified";
-        SearchEngines = {
-          Add = [
-            {
-              Name = "Google Dark";
-              URLTemplate = "https://www.google.com/search?client=firefox-b-d&hl=en&safe=off&q={searchTerms}";
-              IconURL = "https://www.google.com/favicon.ico";
-            }
-          ];
-          Default = "Google Dark";
-          PreventInstalls = true;
-          Remove = [ "Google" "Bing" "DuckDuckGo" "Wikipedia (en)" ];
-        };
-        ExtensionSettings = {
-          "*".installation_mode = "blocked";
-          # Spirited Away Theme
-          "{49aa8e5f-f9d6-4556-a881-010b048e8636}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/spirited-away-animated/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
-          };
-          # uBlock Origin:
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
-          };
-          # Privacy Badger:
-          "jid1-MnnxcxisBPnSXQ@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
-          };
-          # Video Speed Controller:
-          "{7be2ba16-0f1e-4d93-9ebc-5164397477a9}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/videospeed/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
-          };
-        };
-        Preferences = { 
-          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-          "extensions.pocket.enabled" = false;
-          "extensions.screenshots.disabled" = true;
-          "browser.topsites.contile.enabled" = false;
-          "browser.formfill.enable" = false;
-          "browser.search.suggest.enabled" = false;
-          "browser.search.suggest.enabled.private" = false;
-          "browser.urlbar.suggest.searches" = false;
-          "browser.urlbar.showSearchSuggestionsFirst" = false;
-          "browser.newtabpage.activity-stream.default.sites" = "";
-          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-          "browser.newtabpage.activity-stream.feeds.snippets" = false;
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
-          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = false;
-          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = false;
-          "browser.newtabpage.activity-stream.showSponsored" = false;
-          "browser.newtabpage.activity-stream.system.showSponsored" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        };
       };
     };
   };
